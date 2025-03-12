6b4a4ee3b461d431ad0e07a819798ba03cd5455a
ayushsotechy <114767075+ayushsotechy@users.noreply.github.com>
2025-03-12 07:26:53 +0000

    updated conversions

--- 01_basics/03_conversionDatatypes.js
+++ 01_basics/03_conversionDatatypes.js
@@ -1,9 +1,46 @@
-let score = "45abc";
+// let score = "33  ";
 
-console.log(typeof score);
+// console.log(typeof score);
 
 
-let scoreInNumber = Number(score);
-console.log(scoreInNumber);
-console.log(typeof scoreInNumber);
-console.log(typeof(NaN));
+// let scoreInNumber = Number(score);
+// console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+// console.log(typeof(NaN));
+
+
+/*
+for Number() method
+"33"=>33
+"33abc"=>NaN
+"abc33"=>NaN
+true=>1
+false=>0
+null=>0
+undefined=>NaN
+" "=>0
+"33 "=>33
+" 33"=>33
+
+*/
+
+let isLoggedIn="ayush";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);  
+
+/*
+for Boolean() method
+"33"=>true
+"33abc"=>true
+"abc33"=>true
+true=>true
+false=>false
+0=>false
+1=>true
+null=>false
+undefined=>false
+" "=>true
+NaN=>false
+*/
